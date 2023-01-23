07c9186260770f0cfe4ec73366f17a222fdfd99c
Ilikebreadstapled <tridenjoe4@gmail.com>
2023-01-23 00:52:07 +0000

    added filename and mode variable

---
+++ main.py
@@ -0,0 +1,4 @@
+fileName = input("Enter the filename: ")
+mode = input("Enter the filemode you want(do help for more info): ")
+
+open(fileName, mode='mode', buffering=- 1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
